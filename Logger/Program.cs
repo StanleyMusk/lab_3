21387cea0b5f87987f73fb4effe697db9d45f871
StanleyMusk <92634559+StanleyMusk@users.noreply.github.com>
2022-04-02 16:06:06 +0200

    Add files via upload

---
+++ Logger/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using Logger.Logger;
+
+namespace Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+
+            
+
+        }
+    }
+}
